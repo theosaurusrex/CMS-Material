c52ac807c02c2e0cce10586f4262e9183f3ccc12
theosaurusrex <theosaurus@gmail.com>
2018-11-20 12:41:33 -0600

    Before routing and page views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Routing
+import { AppRoutingModule } from './app-routing.module';
+// material section
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+// import { CustomersComponent } from './customers/customers.component';
+// import { OrdersComponent } from './orders/orders.component';
+// import { CustomerListComponent } from './customers/customer-list/customer-list.component';
+// import { OrderListComponent } from './orders/order-list/order-list.component';
+// import { MessageListComponent } from './messages/message-list/message-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // CustomersComponent,
+    // OrdersComponent,
+    // CustomerListComponent,
+    // OrderListComponent,
+    // MessageListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // routing
+    // AppRoutingModule,
+    // Material
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
